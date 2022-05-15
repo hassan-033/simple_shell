d39bf7a6f7bd9e4186e7251adf67b77bfbd61e1b
hassan-033 <99290640+hassan-033@users.noreply.github.com>
2022-05-15 07:55:52 +0100

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/*libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+/*main.c*/
